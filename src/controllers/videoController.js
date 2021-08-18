dea453753e6e4c318854b21584da161e658232b6
Jihyeok-Um <75592315+Jihyeok-Um@users.noreply.github.com>
2021-08-19 00:08:12 +0900

    add mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,13 +1,18 @@
-export const trending = (req,res) => res.render("home", {pageTitle :"home"});
+const videos = [1,2,3,4,5];
+export const trending = (req,res) => res.render("home", {pageTitle:"home", videos});
+
 export const watch = (req,res) => {
     return res.send("Watch");
 }
+
 export const editVideo = (req,res) => {
     return res.send("edit video");
 }
+
 export const removeVideo = (req,res) => {
     return res.send("remove video");
 }
+
 export const upload = (req,res) => {
     return res.send("upload");
 }
