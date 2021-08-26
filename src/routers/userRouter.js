c1c66916bec932e8bc49f8e7237f2f938657e0f9
Jihyeok-Um <75592315+Jihyeok-Um@users.noreply.github.com>
2021-08-26 18:51:51 +0900

    github login (null 오류 수정하기)

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { editUser, removeUser } from "../controllers/userController";
+import { editUser, removeUser, logOutUser ,startGithubLogin, finishGithubLogin } from "../controllers/userController";
 
 const userRouter = express.Router();
 userRouter.get("/edit", editUser);
 userRouter.get("/remove", removeUser);
+userRouter.get("/logout", logOutUser);
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
+
 
 export default userRouter;
